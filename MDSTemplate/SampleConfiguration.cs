6c55484b43562b2731da1e6f07785cae91405e78
MDSEva <seteam@outlook.kr>
2017-08-09 17:27:34 +0900

    mds UWP template

---
+++ MDSTemplate/SampleConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace MDSTemplate
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "MDS Demo";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() {Title = "Scenario 1", ClassType = typeof(Scenario1)},
+            new Scenario() {Title = "Scenario 2", ClassType = typeof(Scenario2)}
+        };
+       
+
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+}
